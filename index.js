359cc4b436da1021d9480e04b62036e6a1ede2b8
Vaishnavi-Chaudhari-10 <vaishnavi.chaudhari1007@gmail.com>
2025-11-12 20:54:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Simple interactivity for Book Now button
+document.getElementById("bookBtn").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Booking feature coming soon! 🚀");
+});
